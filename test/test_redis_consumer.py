edb40a77af288b369203c231e9965e125a90bcb2
codecakes <akulmat@proton.me>
2023-01-12 11:53:47 +0530

    test_redis_setup successfully returns a query key

--- test/test_redis_consumer.py
+++ test/test_redis_consumer.py
@@ -1,11 +1,13 @@
 """Integration-Test suite for redis consumer."""
 import pytest
 import logging
+from core.interface import type_consumer_id
 
 # **************************************************** #
 # Use pytest -m fast to run the tests in fast mode.
 # Use pytest -s to see the test debug output like print.
 # **************************************************** #
+
 
 @pytest.mark.slow
 @pytest.mark.usefixtures("anyio_backend")
@@ -14,10 +16,12 @@ import logging
 class TestRedisConsumer:
     """Test the redis consumer."""
 
-    def test_redis_setup(self, caplogger, redis_session) -> None:
+    async def test_redis_setup(self, caplogger, redis_session) -> None:
         """Test the redis consumer setup."""
         redis_session.redis_conn.ping()
-        assert 1
+        # To bypass worker, see: https://python-rq.org/docs/#bypassing-workers
+        consumer_queue_key = await redis_session.setup(channel="radio", is_async=False)
+        assert isinstance(consumer_queue_key, str)
 
     def test_redis_consume(self) -> None:
         """Test the redis consumer."""
