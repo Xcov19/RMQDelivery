9782027ccc32f9d0c9f4df8575b55bd248824d58
codecakes <akulmat@proton.me>
2023-01-12 11:12:33 +0530

    added redis connection setUp and tearDown fixture redis_session

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,14 @@
+import os
 import logging
 from typing import Any
 
 import pytest
 from pytest import FixtureRequest
 
+from infrastructure.redis_lib import RedisConsumer
+
+pytest_logger = logging.getLogger(__file__)
+pytest_logger.setLevel(logging.INFO)
 
 # ************************************************* #
 # Setup pytest anyio fixture.
@@ -26,7 +31,24 @@ def anyio_backend(request: FixtureRequest.session) -> Any:  # type: ignore[no-an
 # Setup a fixture caplog for unsuccessful tests.
 # You can run pytest -ra -q to see only unsuccessful logs.
 # ************************************************* #
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="function")
 def caplogger(caplog: Any) -> Any:
     caplog.set_level(logging.ERROR, logger="test.test_redis_consumer")
     return caplog
+
+# ************************************************* #
+# Setup redis connection
+# ************************************************* #
+@pytest.fixture(scope="function")
+def redis_session(caplogger):
+    caplogger.set_level(logging.DEBUG, logger="test.test_redis_consumer")
+    kwargs = {}
+    if redis_pwd := os.getenv("REDIS_PASSWORD", None):
+        kwargs |= dict(password=redis_pwd)
+    pytest_logger.info("starting redis consumer session.")
+    print("starting redis consumer session.")
+    redis_consumer = RedisConsumer(**kwargs)
+    yield redis_consumer
+    pytest_logger.info("closing redis session.")
+    print("closing redis session.")
+    redis_consumer.close()
--- test/test_redis_consumer.py
+++ test/test_redis_consumer.py
@@ -1,6 +1,6 @@
 """Integration-Test suite for redis consumer."""
 import pytest
-
+import logging
 
 # **************************************************** #
 # Use pytest -m fast to run the tests in fast mode.
@@ -10,11 +10,13 @@ import pytest
 @pytest.mark.slow
 @pytest.mark.usefixtures("anyio_backend")
 @pytest.mark.usefixtures("caplogger")
+@pytest.mark.usefixtures("redis_session")
 class TestRedisConsumer:
     """Test the redis consumer."""
 
-    def test_redis_setup(self) -> None:
+    def test_redis_setup(self, caplogger, redis_session) -> None:
         """Test the redis consumer setup."""
+        redis_session.redis_conn.ping()
         assert 1
 
     def test_redis_consume(self) -> None:
